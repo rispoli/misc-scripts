464aac008e1824fd17d6e83b6e2b08928d14cc5e
Daniele Rispoli <daniele.rispoli@gmail.com>
2014-12-01 14:22:36 +0100

    Changed path to income file.

--- compute_income_percentage.sh
+++ compute_income_percentage.sh
@@ -3,7 +3,7 @@
 declare -r bills=$(tail -n+2 ~/Dropbox/bills | cut -d, -f2 | paste -sd+ | bc)
 declare -r expenses=$(cat ~/Dropbox/money | cut -d, -f2 | paste -sd+ | bc)
 declare -r tickets=$(cat ~/Dropbox/tickets | cut -d, -f2- | sed -e 's/,/*/' | paste -sd+ | bc)
-declare -r income=$(cat ~/projects/banca/income | cut -d, -f2 | paste -sd+ | bc)
+declare -r income=$(cat ~/Dropbox/projects/banca/income | cut -d, -f2 | paste -sd+ | bc)
 declare -r total_expenses=$(echo "$bills + $expenses - $tickets" | bc)
 declare -r percentage=$(echo "($total_expenses * 100) / $income" | bc)
 
